7200b67892b24ee3ad6c4d0f7aed07b31ae30f0c
tom-con <tconchie@gmail.com>
2017-08-02 21:48:16 -0600

    Adds player logic and tile control logic
    Adds a list of players (initially at 2) that have flags to show board control. Adds logic to update tiles to reflect player actions.

---
+++ Tic Tac Toe/Assets/Scripts/PlayersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayersController : MonoBehaviour {
+
+	public Material[] flags;
+	public int numPlayers;
+
+	private List<Hashtable> players;
+
+	void Start () {
+		players = new List<Hashtable> ();
+		CreatePlayers ();
+	}
+
+	void CreatePlayers(){
+		for(int x = 1; x <= numPlayers; x++){
+			Hashtable newPlayer = new Hashtable ();
+			newPlayer.Add ("number", x);
+			newPlayer.Add ("material", flags[x-1]);
+			players.Add (newPlayer);
+		}
+	}
+}
